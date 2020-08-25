b2c0b91695d018d15603b735f3b76fb04e1bdf8f
Nicole <nicole.franca.goncalves@gmail.com>
2020-08-25 14:32:41 -0300

    instalações

--- api/index.js
+++ api/index.js
@@ -1,1 +1,16 @@
-// scripts aqui
+import express from 'express'
+import bodyParser from 'body-parser'
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:false }));
+
+const port = process.env.PORT || 3000
+
+app.get('*', (req, res) => res.status(200).send({
+    message: 'Hello word'
+}))
+
+app.listen(port, () => {
+    console.log(`Server is running on PORT ${port}`)
+})
